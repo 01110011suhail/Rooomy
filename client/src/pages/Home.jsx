8f921d19f3c78845fdefc5dae5ce2b29015716cd
Md Suhail <sha433415@gmail.com>
2025-12-01 22:50:13 +0530

    Hotel_card updates

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Navbar from '../components/navbar.jsx'
+import FeaturedDestination from '../components/FeaturedDestination.jsx'
 
 const Home = () => {
   return (
     <>
         <Hero />
+        <FeaturedDestination />
     </>
   )
 }
