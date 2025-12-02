fd7c1a2b3e42ffff029f0e9abf59f42d0d61796c
Md Suhail <sha433415@gmail.com>
2025-12-02 19:04:01 +0530

    Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import Navbar from './components/navbar.jsx';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
+import Footer from './components/Footer.jsx';
 
 const App = () => {
   const location = useLocation();
   const isOwnerPath = location.pathname.includes("owner");
 
   return (
-    <>
+    <div>
       {!isOwnerPath && <Navbar />}
-      <Routes>
+      <div className='min-h-[70vh]'>
+        <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/owner" element={<OwnerDashboard />} /> */}
       </Routes>
-    </>
+      </div>
+      <Footer />  
+
+    </div>
   );
 };
 
