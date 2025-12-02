b0dd7cb8922080cbedcf3843c38d2b445cf1d778
Md Suhail <sha433415@gmail.com>
2025-12-02 23:20:07 +0530

    rooms page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/navbar.jsx';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Footer from './components/Footer.jsx';
+import AllRooms from './pages/AllRooms.jsx';
 
 const App = () => {
   const location = useLocation();
@@ -14,6 +15,7 @@ const App = () => {
       <div className='min-h-[70vh]'>
         <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/rooms" element={<AllRooms />} />
         {/* <Route path="/owner" element={<OwnerDashboard />} /> */}
       </Routes>
       </div>
