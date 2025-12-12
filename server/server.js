2fa56815f75b130455fcbd48d279462ce902e069
Md Suhail <sha433415@gmail.com>
2025-12-12 23:26:53 +0530

    Db connection

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from "express"
+import "dotenv/config";
+import cors from "cors";
+const app =express()
+app.use(cors())
+
+app.get('/',(req,res)=> res.send("API is Working"))
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> console.log(`server running on port ${PORT}`));
+
